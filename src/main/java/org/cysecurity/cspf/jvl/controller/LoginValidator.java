01573ec411a98705c4cee7d96e159729fe9f4432
ksalman-Cx <104396358+ksalman-Cx@users.noreply.github.com>
2022-06-03 13:31:59 -0500

    Update LoginValidator.java

--- src/main/java/org/cysecurity/cspf/jvl/controller/LoginValidator.java
+++ src/main/java/org/cysecurity/cspf/jvl/controller/LoginValidator.java
@@ -18,8 +18,14 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import org.cysecurity.cspf.jvl.model.DBConnect;
- 
- 
+
+/*------Insert the 1 line below to line 21-----
+
+import java.sql.PreparedStatement;
+
+---------------------------------------------*/
+
+
 
 /**
  *
@@ -47,8 +53,21 @@ public class LoginValidator extends HttpServlet {
                  Connection con=new DBConnect().connect(getServletContext().getRealPath("/WEB-INF/config.properties"));
                     if(con!=null && !con.isClosed())
                                {
+                     /*------ Uncomment the below 6 lines for SQL Fix ------*/
+                     
+                     /*
+                                     ResultSet rs = null;
+                                     String sql = "select * from users where username=? and password=?"; 
+                                     PreparedStatement preparedStatement = con.prepareStatement(sql);
+                                     preparedStatement.setString(1,user);
+                                     preparedStatement.setString(2,pass);
+                                     rs = preparedStatement.executeQuery(); 
+                     */
+                     
+                     /*------ Comment out the two lines below for SQL Fix ------*/
                                    ResultSet rs=null;
                                    Statement stmt = con.createStatement();  
+                     /*---------------------------------------------------------*/  
                                    rs=stmt.executeQuery("select * from users where username='"+user+"' and password='"+pass+"'");
                                    if(rs != null && rs.next()){
                                    HttpSession session=request.getSession();
