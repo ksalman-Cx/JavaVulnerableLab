842064c78db0fcac3afcfe18312a6fa378a651f0
ksalman-cx <kelsey.salman@checkmarx.com>
2022-09-12 15:39:31 -0500

    2 sca fixes and 1 kics fix

--- s3.tf
+++ s3.tf
@@ -1,6 +1,12 @@
 resource "aws_s3_bucket" "b" {
   bucket = "S3B_181355"
   acl    = "public-read"
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm     = "AES256"
+      }
+  }
 
   tags = {
     Name        = "My bucket"
